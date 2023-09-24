bc1b8bd9dddb6453faf0bc2b3a2f9099fe3be259
Ryan Tong <baotongxuan@gmail.com>
2023-09-25 03:11:29 +1000

    Add list of blogs

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,3 +1,30 @@
-export default function BlogsPage() {
-  return <div>Blogs Page</div>;
+import { allPosts } from "contentlayer/generated";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+
+export default async function Page() {
+  return (
+    <div className="grid gap-4 sm:grid-cols-2 py-4">
+      {allPosts.map((post) => (
+        <article
+          key={post.slugAsParams}
+          className="group relative border-solid border-2 rounded-md bg-secondary p-4"
+        >
+          <header className="pb-4">
+            <h1>{post.title}</h1>
+            <small className="text-slate-600 dark:text-slate-500">
+              {formatDate(post.date)}
+            </small>
+          </header>
+          <p className="text-sm">{post.description}</p>
+          <Link
+            href={`/blog/${post.slugAsParams}`}
+            className="absolute inset-0"
+          >
+            <span className="sr-only">View Article</span>
+          </Link>
+        </article>
+      ))}
+    </div>
+  );
 }
